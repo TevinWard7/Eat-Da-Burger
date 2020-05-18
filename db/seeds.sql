43681ea5995e11dd18f020c8a009e8f4be6f3fb6
Tevin <tevinward7@gmail.com>
2020-05-17 22:20:04 -0400

    added seeds.sql and 3 insert statements

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured) VALUES (whopper, false);
+
+INSERT INTO burgers (burger_name, devoured) VALUES (big mac, false);
+
+INSERT INTO burgers (burger_name, devoured) VALUES (strauss, true);
