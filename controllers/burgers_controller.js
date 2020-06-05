464fb14b3a3f0ddaa75804df3393e19e9e66aa81
Tevin <tevinward7@gmail.com>
2020-06-05 03:49:49 -0400

    edited / route

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,11 +3,15 @@ const burger = require("../models/burger.js");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-    const burgers = {
-        burgers: burger.selectAll
-    }
-    console.log(burgers)
-    res.render("index", burgers);
+
+    burger.selectAll((data) => {
+        const hbsobject = {
+            burgers: data
+        };
+        console.log("hbsobject " + hbsobject)
+        res.render("index", hbsobject);
+    });
+
 });
 
 
